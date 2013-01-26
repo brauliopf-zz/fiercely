b642310ea2de0735833fe188c6e9e3577f2b9c96
Braulio Fernandes <brauliopf@gmail.com>
2013-01-25 17:02:20 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiercely::Application.config.secret_token = '24a12e57ffbd335bb3f4e83fa694d2fb21060c77331813ea6ec307909fdeb5f1405a81ee83b5a5409006eb6580fe4a2373ff6fa99761d32d62597594c6d4bbca'
